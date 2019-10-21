b0b8f20ba36177b536380897d8315683bf19449f
Bradley Zylstra <bzylstra@umass.edu>
2019-10-21 15:10:21 -0400

    Implemented chat server

--- Lab3/chat_server.py
+++ Lab3/chat_server.py
@@ -19,15 +19,17 @@ while True:
         if data.find('public_key')!=-1: #client has sent their public key\
             ###################################your code goes here#####################################
             #retrieve public key and private key from the received message (message is a string!)
-            public_key_e=0
-            public_key_n=0
+            subs = data.find(": ") + 2
+            new_str = data[subs:]
+            public_key_e = int(new_str[0: new_str.find(" ")])
+            public_key_n = int(new_str[new_str.find(" ")+1: ])
             print ('public key is : %d, %d'%(public_key_e,public_key_n))
         else:
             cipher=int(data)
             print (str(cipher)+':')
             ###################################your code goes here#####################################
             #data_decoded is the decoded character based on the received cipher, calculate it using functions in RSA.py
-            data_decoded='a'
+            data_decoded=decrypt(data)
             print (data_decoded)
                 #python2: print data ,
 sys.ext()
