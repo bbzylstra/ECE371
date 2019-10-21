3b1d22b4aa4a4cf7b7b1e588933a77e4df687128
Bradley Zylstra <bzylstra@umass.edu>
2019-10-21 08:45:59 -0400

    Added lab3 code

---
+++ Lab3/RSA.py
@@ -0,0 +1,62 @@
+
+import random
+
+
+#fnction for finding gcd of two numbers using euclidean algorithm
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+#uses extened euclidean algorithm to get the d value
+#for more info look here: https://crypto.stackexchange.com/questions/5889/calculating-rsa-private-exponent-when-given-public-exponent-and-the-modulus-fact
+#will also be explained in class
+def get_d(e, z):
+    ###################################your code goes here#####################################
+    d=0
+    return d
+    
+def is_prime (num):
+    if num > 1: 
+      
+        # Iterate from 2 to n / 2  
+       for i in range(2, num//2): 
+         
+           # If num is divisible by any number between  
+           # 2 and n / 2, it is not prime  
+           if (num % i) == 0: 
+               return False 
+               break
+           else: 
+               return True 
+  
+    else: 
+        return False
+
+
+def generate_keypair(p, q):
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError('Both numbers must be prime.')
+    elif p == q:
+        raise ValueError('p and q cannot be equal')
+    ###################################your code goes here#####################################
+
+    e=0
+    n=0
+    d=0
+    return ((e, n), (d, n))
+
+def encrypt(pk, plaintext):
+    ###################################your code goes here#####################################
+    #plaintext is a single character
+    #cipher is a decimal number which is the encrypted version of plaintext
+    #the pow function is much faster in calculating power compared to the ** symbol !!!
+    cipher=0;
+    return cipher
+
+def decrypt(pk, ciphertext):
+    ###################################your code goes here#####################################
+    #ciphertext is a single decimal number
+    #the returned value is a character that is the decryption of ciphertext
+    plain='a'
+    return ''.join(plain)
